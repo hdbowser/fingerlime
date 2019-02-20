7098a3af0100bbc50e814f4134c9583f228f642a
Victor M. Brito <hdbowser@gmail.com>
2019-02-19 23:47:12 -0400

    Se termino el modelado de la base de datos y el diseño de la vista principal está casi terminado

---
+++ fingerlime/src/fingerlime/App.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fingerlime;
+
+import fingerlime.gui.MainForm;
+import javax.swing.UIManager;
+
+/**
+ *
+ * @author victor
+ */
+public class App {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+
+        try {
+            for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            // If Nimbus is not available, you can set the GUI to another look and feel.
+        }
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new MainForm().setVisible(true);
+            }
+        });
+    }
+
+}
